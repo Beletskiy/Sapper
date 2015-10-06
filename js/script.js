44c0e351fbb694e799b28177a81f759270eb7ad4
Beletskiy <Alexey1Beletskiy@gmail.com>
2015-10-06 19:45:02 +0300

    6_10_1

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
-function Game () {
-    this.drawer = new HTMLDrawer();
-    //this.drawer = new CanvasDrawer();
-    // this.drawer.drawField (this.field);
-    this.modelArr = null;
-  //  this.modelObj = null;
-    this.size = 0;
 
-}
 /*--------------------------------------------------visualization-------------------------------------------*/
-function HTMLDrawer () {
-  this.htmlField = null;
+function HTMLDrawer (leftClickHandler, rightClickHandler) {
+    this.htmlField = null;
+    this.leftClickHandler = leftClickHandler;
+    this.rightClickHandler = rightClickHandler;
 }
 
 function CanvasDrawer () {
@@ -19,34 +13,37 @@ function CanvasDrawer () {
 HTMLDrawer.prototype.drawHtmlField = function (modelArr) {
    var htmlField = modelArr,
        table = document.createElement('table'),
-       self = this,
-       tr = null, td = null;
+       tr = null, td = null,
+       self = this;
 
-    for (var i=0; i<htmlField.length; i++) {
+    for (var i = 0; i < htmlField.length; i++) {
         tr = document.createElement('tr');
         table.appendChild(tr);
-        for (var j=0; j<htmlField.length; j++) {
+        for (var j = 0; j < htmlField.length; j++) {
             td = document.createElement('td');
             tr.appendChild(td);
             td.setAttribute('id', 'c_'+j+'_'+i);
             td.setAttribute('class', 'closed');
-            var leftClickHandler = function(e){
+
+             var leftClickHandler = function(e){
                 var el = e.currentTarget,
                     id = el.getAttribute('id'),
                     parts = id.split('_'),
                     x = parts[1],
                     y = parts[2];
-                Game.prototype.onCellClick(x, y,modelArr);
+                self.leftClickHandler(x, y, modelArr);
             };
-            var rightClickHandler = function (e) {
+
+             var rightClickHandler = function (e) {
                 var el = e.currentTarget,
                     id = el.getAttribute('id'),
                     parts = id.split('_'),
                     x = parts[1],
                     y = parts[2];
-                Game.prototype.onCellRightClick(x, y,modelArr);
+                self.rightClickHandler(x, y, modelArr);
                 e.preventDefault();
-            }
+            };
+
             td.addEventListener('click', leftClickHandler);
             td.addEventListener('contextmenu', rightClickHandler);
         }
@@ -56,6 +53,7 @@ HTMLDrawer.prototype.drawHtmlField = function (modelArr) {
 
 HTMLDrawer.prototype.showBomb = function (x,y,modelArr) {
     var htmlField = modelArr,
+        self = this,
         cellId = 'c_'+x+'_'+y;
 
     document.getElementById(cellId).classList.remove('closed');
@@ -64,18 +62,16 @@ HTMLDrawer.prototype.showBomb = function (x,y,modelArr) {
     for (var i = 0; i < htmlField.length; i++) {
         for (var j = 0; j < htmlField.length; j++) {
             cellId = 'c_'+j+'_'+i;
-            document.getElementById(cellId).
-                removeEventListener("click", HTMLDrawer.prototype.drawHtmlField['leftClickHandler']); // не работает, объявить handler через prototype ?
             if (Game.prototype.isBomb(j,i)) {
                 document.getElementById(cellId).classList.add('bomb');
             }
         }
     }
+    alert ('You lose!');
 };
 HTMLDrawer.prototype.showNumber = function (x,y,modelArr) {
     var htmlField = modelArr,
         cellId = 'c_'+x+'_'+y;
-
     document.getElementById(cellId).classList.remove('closed');
     document.getElementById(cellId).innerHTML = htmlField[x][y][0];
 
@@ -85,12 +81,13 @@ HTMLDrawer.prototype.showBlank = function (x, y, modelArr) {
         cellId = 'c_'+x+'_'+y;
     document.getElementById(cellId).classList.remove('closed');
 };
-HTMLDrawer.prototype.showFlag = function (x, y, modelArr) {
+HTMLDrawer.prototype.showFlag = function (x, y) {
     var cellId = 'c_'+x+'_'+y;
     document.getElementById(cellId).classList.remove('closed');
     document.getElementById(cellId).classList.add('flag');
+
 };
-HTMLDrawer.prototype.disableFlag = function (x, y, modelArr) {
+HTMLDrawer.prototype.disableFlag = function (x, y) {
     var cellId = 'c_'+x+'_'+y;
     document.getElementById(cellId).classList.remove('flag');
     document.getElementById(cellId).classList.add('closed');
@@ -99,10 +96,27 @@ HTMLDrawer.prototype.disableFlag = function (x, y, modelArr) {
 /*----------------------------------------------end of visualization----------------------------------------*/
 
 /*---------------------------------------------------logic--------------------------------------------------*/
+function Game () {
+    //this.drawer = new HTMLDrawer();
+
+    //var self = this;
+    //this.drawer = new HTMLDrawer(function () {
+    //    self.onCellClick()
+    //});
+
+    this.drawer = new HTMLDrawer(this.onCellClick.bind(this), this.onCellRightClick.bind(this));
+
+    console.log(this.drawer);
+    //this.drawer = new CanvasDrawer();
+    // this.drawer.drawField (this.field);
+    this.modelArr = null;
+    this.size = 0;
+
+}
+
 Game.prototype.start = function(s) {  //create model of the square field , w - side of the square
     this.modelArr = [];
     this.size = s;
-  //  this.modelObj = {};
     var self = this;
     for (var i=0; i<this.size; i++) {
         var t = [];
@@ -176,7 +190,7 @@ Game.prototype.arrangeNumbers = function (y,x) {
         this.modelArr[y + obj.y][x + obj.x][0]++;
     }
 }
-    console.log(this.modelArr);
+   // console.log(this.modelArr);
 };
 
 Game.prototype.getRand = function (min, max) {
@@ -192,36 +206,63 @@ Game.prototype.isNumber = function (x, y) {
         return true;
     }
 };
+Game.prototype.isFlag = function (x,y) {
+    if (this.modelArr[x][y][1] == 'flag') {
+        return true;
+    }
+};
 Game.prototype.onCellClick = function (x, y, modelArr) {
     var self = this;
     this.modelArr = modelArr;
-    if (self.isBomb(x, y)) {
-        HTMLDrawer.prototype.showBomb(x,y,this.modelArr); //!!! сделать динамически HTML или Canvas, проверять объект какого типа создан ?
-    } else if (self.isNumber(x, y)) {
+    console.log(this.drawer);
+   // this.drawer = new HTMLDrawer();
+    if ((self.isBomb(x, y)) && (!self.isFlag(x, y))) {
+      //  HTMLDrawer.prototype.showBomb(x,y,this.modelArr);   ----???----
+        this.drawer.showBomb(x,y,this.modelArr);
+    } else if ((self.isNumber(x, y)) && (!self.isFlag(x, y))) {
         HTMLDrawer.prototype.showNumber(x,y,this.modelArr);
-    } else {
+        this.modelArr[x][y][1] = 'open';
+    } else if (!self.isFlag(x, y)) {
         HTMLDrawer.prototype.showBlank(x,y,this.modelArr);
+        this.modelArr[x][y][1] = 'open';
     }
-
+    if (self.isWin(this.modelArr)) {
+        alert('You Win!!!');
+    }
 };
 Game.prototype.onCellRightClick = function (x, y, modelArr) {
-    var state = null;
+    var state;
     this.modelArr = modelArr;
         state = this.modelArr[x][y][1];
     if (state == 'closed') {
         this.modelArr[x][y][1] = 'flag';
-        HTMLDrawer.prototype.showFlag(x,y,this.modelArr);
+        HTMLDrawer.prototype.showFlag(x,y);
     } else if (state == 'flag') {
         this.modelArr[x][y][1] = 'closed';
-        HTMLDrawer.prototype.disableFlag(x,y,this.modelArr);
+        HTMLDrawer.prototype.disableFlag(x,y);
     }
 
-
 };
+Game.prototype.isWin = function (modelArr) {
+    this.modelArr = modelArr;
+    var numberOpenCells = 0,
+        size = this.modelArr.length;
 
+    for (var i = 0; i < size; i++) {
+        for (var j = 0; j < size; j++) {
+            if (this.modelArr[j][i][1] == 'open'){
+                numberOpenCells++;
+                if (numberOpenCells == size*size - size) {
+                    return true;
+                }
+            }
+        }
+    }
+};
 
 /*--------------------------------------end of logic------------------------------------------*/
 var game = new Game();
+//var htmlDrawer = new HTMLDrawer();
 game.start(3);
 game.drawer.drawHtmlField(game.modelArr);
 
